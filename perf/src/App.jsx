b4d116c1971211a44d81c9f60ace8978af8d7701
Raven Paragas <ravenp.dev@gmail.com>
2026-07-24 15:04:43 +0800

    chore: react performance optimization
    learn how to use memo, useMemo, useCallback and how react compiler can help performance optimization

---
+++ perf/src/App.jsx
@@ -0,0 +1,49 @@
+import { useCallback, useEffect, useMemo, useState } from "react";
+import { marked } from "marked";
+
+import MarkdownPreview from "./MarkdownPreview";
+import markdownContent from "./markdownContent";
+
+export default function App() {
+  const [text, setText] = useState(markdownContent);
+  const [time, setTime] = useState(Date.now());
+  const [theme, setTheme] = useState("green");
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime(Date.now());
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  const options = useMemo(() => {
+    return { text, theme };
+  }, [text, theme]);
+  // const render = useMemo(() => (text) => marked.parse(text), []);
+  const render = useCallback((text) => marked.parse(text), []);
+
+  return (
+    <div className="app">
+      <h1>Perf with React</h1>
+      <h2>Current Time: {time}</h2>
+      <label htmlFor="theme">
+        Choose a theme
+        <select onChange={(e) => setTheme(e.target.value)}>
+          <option value="green">Green</option>
+          <option value="blue">Blue</option>
+          <option value="yellow">Yellow</option>
+          <option value="red">Red</option>
+        </select>
+      </label>
+      <div className="markdown">
+        <textarea
+          className="markdown-editor"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        ></textarea>
+        <MarkdownPreview options={options} render={render} />
+      </div>
+    </div>
+  );
+}
